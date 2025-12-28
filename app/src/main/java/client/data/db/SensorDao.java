08408b185e921c19abf60bbaf8fb140bf511369c
Daniil Khamidullin <115153073+0408raku3n@users.noreply.github.com>
2025-12-28 18:49:18 +0500

    add dao sensor

---
+++ app/src/main/java/client/data/db/SensorDao.java
@@ -0,0 +1,9 @@
+@Dao
+public interface SensorDao {
+
+    @Query("SELECT * FROM SensorHistory ORDER BY timestamp DESC")
+    List<SensorHistory> getAll();
+
+    @Insert
+    void insertAll(List<SensorHistory> list);
+}
